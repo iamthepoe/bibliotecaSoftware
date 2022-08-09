711f265091ae82c7b47635b56fcd025afdd799d9
furPedroRibeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2022-08-08 22:16:20 -0300

    ajeitando pastas e fazendo section card

---
+++ php/cadastrar.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- links -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Rubik&family=Square+Peg&display=swap"
+      rel="stylesheet"
+    />
+
+    <style>
+      *{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+:root{
+  --corPrincipal: rgb(107, 191, 164);
+  --textColor: rgb(237, 236, 227);
+}
+body{
+  background-color: var(--corPrincipal);
+}
+.tamanho{
+  width: 60vw;
+  margin-inline: auto;
+}
+header, .header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 12vh;
+  width: 100%;
+  color: var(--textColor);
+
+  font-family: 'Rubik', sans-serif;
+  font-family: 'Square Peg', cursive;
+  font-size: 60px;
+}
+.main{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  height: 70vh;
+}
+.main, .img{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin-block-end: 3vh;
+}
+.main, .inputs{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 2.5vh;
+}
+input{
+  border: none;
+  border-bottom: 1px solid whitesmoke;
+  padding: 0.7vw;
+
+  transition: width 0.3s ease-in-out;
+
+  width: 90%;
+
+  background-color: var(--corPrincipal);
+  color: white;
+  outline: none;
+}
+input:focus{
+  width: 100%;
+}
+::placeholder{
+  color: whitesmoke;
+}
+main form #forgetPassword{
+  color: #0ba360;
+}
+.bn632-hover {
+  width: 54vw;
+  font-size: 16px;
+  font-weight: 600;
+  color: #fff;
+  cursor: pointer;
+  height: 6vh;
+  text-align:center;
+  border: none;
+  background-size: 300% 100%;
+  border-radius: 50px;
+  moz-transition: all .4s ease-in-out;
+  -o-transition: all .4s ease-in-out;
+  -webkit-transition: all .4s ease-in-out;
+  transition: all .4s ease-in-out;
+}
+
+.bn632-hover:hover {
+  background-position: 100% 0;
+  moz-transition: all .4s ease-in-out;
+  -o-transition: all .4s ease-in-out;
+  -webkit-transition: all .4s ease-in-out;
+  transition: all .4s ease-in-out;
+  transition: width 0.3s ease-in-out;
+  width: 60vw;
+}
+
+.bn632-hover:focus {
+  outline: none;
+}
+
+.bn632-hover.bn22 {
+  background-image: linear-gradient(
+    to right,
+    #0ba360,
+    #3cba92,
+    #30dd8a,
+    #2bb673
+  );
+  box-shadow: 0 4px 15px 0 rgba(23, 168, 108, 0.75);
+}
+    </style>
+
+    <title>Login</title>
+  </head>
+  <body>
+    <div class="tamanho">
+    <header class="header">
+      <div class="textLogo"><span>AdInfinitum</span></div>
+    </header>
+    </div>
+    <main class="main">
+      <div class="tamanho">
+      <div class="img">
+        <img src="img/bookLog.png" alt="Livro svg" width="125px" />
+      </div>
+      <form action="" class="inputs">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite seu email:"
+        />
+        <input type="text" name="rm" id="rm" placeholder="Digite seu RM:">
+          <input type="text" name="nome" id="nome" placeholder="Digite seu nome:">
+        <input
+          type="password"
+          name="senha"
+          id="senha"
+          placeholder="Digite sua senha:"
+        />
+        <a href="#" id="forgetPassword">Esqueceu sua senha?</a>
+        <a href="pageMain.html"><button class="bn632-hover bn22">Entrar</button></a>
+      </form>
+    </div>
+    </main>
+  </body>
+</html>
